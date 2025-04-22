819f8cb7620ed1dd5c718926cad0ae13c29249c6
Dakodah Spurlin <dcspurlin@gmail.com>
2025-04-22 12:43:19 -0500

    Header has been created.

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+// import { useState, useEffect } from "react";
+import './Header.css';
+function Header() {
+
+    return(
+        <div className='header-div'>
+            <h1 className='main-header'>Question Everything, Everyone</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/_template/_template.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+function _template() {
+
+    return(
+        <div>
+            <p>This is a template. </p>
+        </div>
+    )
+}
